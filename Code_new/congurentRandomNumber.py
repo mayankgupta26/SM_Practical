16565a7bc24e95acd271db5afa7b7747f6823aae
Mayank Gupta <60346019+mayankgupta26@users.noreply.github.com>
2021-04-08 10:18:58 +0530

    Add files via upload

---
+++ Code_new/congurentRandomNumber.py
@@ -0,0 +1,18 @@
+#Mayank GUPTA
+#Roll NO:-1/17/FET/BCS/026
+from random import randrange
+randomNums=[0]*10	
+randomNums[0] = 5 
+
+
+	
+for i in range(1, 10): 
+		
+	randomNums[i] = ((randomNums[i - 1] * 3) +
+										3) % 7 
+print("Random Number using congurent")
+for i in randomNums: 
+	print(i, end = " ") 
+print("\nRandom number using built-in functions")
+for i in range(10):
+    print(randrange(10),end=" ")
